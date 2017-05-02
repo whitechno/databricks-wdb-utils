39e58223e313c59408a90d4113ff17652123f246
owhite <owhite@conviva.com>
2017-05-02 13:35:06 -0700

    add WDB-Setup

---
+++ DBN/Lib/WDB-Setup.scala.scala
@@ -0,0 +1,10 @@
+// Databricks notebook source
+// %run Lib/WDB-Setup.scala
+
+// COMMAND ----------
+
+// MAGIC %run ./WDB-Utils.scala
+
+// COMMAND ----------
+
+import com.whitechno.databricks.utils.wdb._
